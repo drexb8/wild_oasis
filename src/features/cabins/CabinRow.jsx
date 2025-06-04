bee87f54786e464726248d30abc54c9e6669615e
Jardrex Bolon <drex.bolon@gmail.com>
2025-06-04 21:57:50 +0800

    completed the delete cabin function using useMutation

--- src/features/cabins/CabinRow.jsx
+++ src/features/cabins/CabinRow.jsx
@@ -1,6 +1,8 @@
 import styled from "styled-components";
 import { formatCurrency } from "../../utils/helpers";
 import Button from "../../ui/Button";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { deleteCabin } from "../../services/apiCabins";
 
 const TableRow = styled.div`
   display: grid;
@@ -41,17 +43,41 @@ const Discount = styled.div`
   color: var(--color-green-700);
 `;
 function CabinRow({ cabin }) {
-  const { name, maxCapacity, regularPrice, discount, description, image } =
-    cabin;
+  const queryClient = useQueryClient();
+  const { isPending: isDeleting, mutate } = useMutation({
+    mutationFn: deleteCabin,
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: ["cabins"],
+      });
+    },
+    onError: (error) => {
+      alert(error.message);
+    },
+  });
+  const {
+    id: cabinId,
+    name,
+    maxCapacity,
+    regularPrice,
+    discount,
+    description,
+    image,
+  } = cabin;
   return (
     <TableRow role="row">
       <Img src={image} />
       <Cabin>{name}</Cabin>
       <div>Fits up to {maxCapacity} guests</div>
       <Price>{formatCurrency(regularPrice)}</Price>
-      <Discount>{formatCurrency(regularPrice)}</Discount>
-      <Button size="small" variation="danger">
-        Delete
+      <Discount>{formatCurrency(discount)}</Discount>
+      <Button
+        size="small"
+        variation="danger"
+        onClick={() => mutate(cabinId)}
+        disabled={isDeleting}
+      >
+        {isDeleting ? "Deleting..." : "Delete"}
       </Button>
     </TableRow>
   );
